e47200c94d5cb664906db5bac6af37ad26f8d16c
jubedoyap <jubedoyap@unal.edu.co>
2024-11-24 14:24:51 -0500

    Se modifica el código de asignatura y grupo
    Se envía el código con la posible solución

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,9 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    def __init__(self, nombre, salon="remoto"):
         self._nombre = nombre
         self._salon = salon
 
-    # def __str__(self):
-    #     pass
+    def __str__(self): #Se crea para poder imprimir las caracterísitcas del objeto cuando se llame
+        cadena = self._nombre +" "+ self._salon
+        return cadena
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,23 +1,25 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = None
+    grado = "Grupo 12" #Es un atributo de clase público (convención) para que al llamarlo desde el main se pueda imprimir
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes=[]): #asignaturas y estudinates se crean como listas 
         self._grupo = grupo
         self._asignaturas = asignaturas
-        self.listadoAlumnos = estudiantes
+        self.listadoAlumnos = estudiantes #Se declara el atributo como público (simbólicamente) ya que se accede desde el main 
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):#Es necesario poner el None, para que se trabaje con diferentes listas según el grupo
+        if lista == None:
+            lista = []
         lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
+        self.listadoAlumnos = lista
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return "Grupo de estudiantes: "+self._grupo #De esta manera cuando se puede imprimir el atributo grupo de un objeto de tipo Grupo, cuando se llame 
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
@@ -28,5 +30,5 @@ class Grupo:
         cls.grado = nombre
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
+    def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
